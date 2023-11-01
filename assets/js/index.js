c4cf61d166a904463e675de77d72487cf2c841fe
AndresFTamara <andrestamara45@gmail.com>
2023-11-01 15:32:26 -0500

    Se ha terminado la parte del footer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,15 @@
+const header = document.getElementById('header')
+addEventListener('scroll', (event) => {
+    if (this.scrollY > 100) {
+        header.classList.add('header--fill')
+    } else {
+        header.classList.remove('header--fill')
+    }
+})
+
+
+
 /*const testimonios = document.querySelectorAll('.elemento_testimonio') 
 testimonios.forEach((testi) => {
     testi.firstElementChild.style.height = `${testi.getBoundingClientRect().height+13} px`
-})
+})*/
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,17 +1,20 @@
 const form = document.getElementById('login_form')
 
 if (form) {
+    console.log('esasfa')
     form.addEventListener('submit', (event) => {
         event.preventDefault()
+        console.log(event);
         const email = event.target.element.email.value
         const password = event.target.element.password.value
-
+        
 
         //email: admin@gmail.com password: admin12345 //
 
         if (email === 'admin@gmail.com' && password === 'admin12345') {
             localStorage.setItem('HAS_USER', 'true')
             window.location = '../views/admin.html'
+            console.log('kkk')
         }
     })
 }
